320ba3e8bf48d87b980e61df0b42c8ddac5e08c5
GithubforBen <95373251+GithubforBen@users.noreply.github.com>
2022-08-11 13:12:41 +0200

    Added OptionsMenu

--- BenTimoGame/Assets/Manager.cs
+++ BenTimoGame/Assets/Manager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +9,47 @@ public class Manager : MonoBehaviour
 {
     public void PlayGame () 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
         FindObjectOfType<AudioManager>().Play("Test_Start");
+    }
+    public void Options() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Close() 
+    {
+        Application.Quit();
+    }
+    public void Menu () 
+    {
+        LoadScene(0);
+    }
+
+
+
+
+    public void LoadScene(int i) 
+    {
+        try
+        {
+            SceneManager.LoadScene(i);
+        }
+        catch (System.Exception)
+        {
+            Debug.LogError("Die Szene " + i + " existiert nicht!");
+            throw;
+        }
+    }
+    public void LoadNextScene() 
+    {
+        try
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        catch (System.Exception)
+        {
+            Debug.LogError("Die Szene existiert nicht!");
+            throw;
+        }
     }
 }
